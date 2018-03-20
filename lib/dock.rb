57b2ccc881c9cbe9e91a0ff6e79ea321296d9f42
Daniel Miller <danielmiller5791@gmail.com>
2018-03-20 16:28:49 +0000

    bike release done

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,25 +1,38 @@
 
   class Bike
     def initialize
-  x = rand(100)
-  @working = x > 10
+      x = rand(100)
+      @working = x > 10
+      @bike = false
   #returns if true
-end
-  def is_working?
-    @working
-  end
-def working
-  @working
-end
+    end
+    def is_working?
+      @working
+    end
+    def working
+      @working
+    end
+
+    def released
+      @bike = true
+    end
+
+    def locked
+      @bike = false
+    end
+
+    def check_bike
+      puts "status of bike is #{@bike}"
+    end
     
   end
 
   class DockingStation
 
-    def release_bike
-      puts "Do you want to release a bike?"
-      ans = gets.chomp
-      ans
+    def release_bike(a_bike)
+      if a_bike.is_working?
+        a_bike.released
+      end
     end
 
    
@@ -27,6 +40,10 @@ end
   end
 
   docking_station = DockingStation.new
+  bike = Bike.new
+
+  if docking_station.release_bike == "yes"
+    
 
 =begin
   if docking_station.release_bike == "yes"
